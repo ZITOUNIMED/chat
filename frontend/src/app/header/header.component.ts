2650f35662a5a0dd5574380e84d3bcd7044c8105
“Mohamed <zitouni.mohamed.email@gmail.com>
2023-04-10 14:24:44 +0200

    Améliorer le design de l'application

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,33 +1,14 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { Observable, map } from 'rxjs';
-import { User } from '../models/user.model';
-import { AppState } from '../store/states/app.state';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  user$: Observable<User>;
-  @Output() signOutchange = new EventEmitter<boolean>();
-
-  constructor(private store: Store<AppState>) { 
-    this.user$ = this.store.select(state => state.user).pipe(
-      map(userState => {
-        const user: User = {
-          username: userState.username,
-          name: userState.name,
-          uuid: userState.uuid
-        };
-
-        return user;
-      })
-    );
+  
+  constructor() { 
   }
-  signOut(): void {
-    this.signOutchange.emit(true);
-  }
+  
 
 }
--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { AppState } from '../store/states/app.state';
+import { Store } from '@ngrx/store';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  user$: Observable<User>;
+  @Output() signOutchange = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { 
+    this.user$ = this.store.select(state => state.user).pipe(
+      map(userState => {
+        const user: User = {
+          username: userState.username,
+          name: userState.name,
+          uuid: userState.uuid
+        };
+
+        return user;
+      })
+    );
+  }
 
   ngOnInit(): void {
   }
 
+  signOut(): void {
+    this.signOutchange.emit(true);
+  }
 }
