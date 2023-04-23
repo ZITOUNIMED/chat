2bb2446d9c4c20f7ca3f1309adee090e4f77c5d2
“Mohamed <zitouni.mohamed.email@gmail.com>
2023-04-23 16:15:02 +0200

    fix app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
@@ -33,7 +32,6 @@ import { MenuComponent } from './menu/menu.component';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    NgbModule,
     StoreModule.forRoot(appReducer),
   ],
   providers: [
